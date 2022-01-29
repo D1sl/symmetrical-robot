d122ddde28ed8602eb85a7eac81aa6876b824093
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-29 09:49:16 -0600

    Hardcoded worst movies by genre and added event listener for the dropdown list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var searchInputEl = document.querySelector("#search");
 var searchButton = document.querySelector("#run-search");
+var genreListEl = document.querySelector("#genre");
 
 var sendSearch = function (event) {
     // Prevent page from reloading which is the browser default action on form submit
@@ -11,6 +12,14 @@ var sendSearch = function (event) {
     // Take the searchQuery variable and use it to redirect the user to the next page, adding the variable to the end of the URL 
     location.href = './actor.html?=' + searchQuery;
 };
+
+var getMoviesByGenre = function (event) {
+    var genre = genreListEl.value;
+    console.log(genre);
+}
+
+// When an option on the list is selected
+genreListEl.addEventListener('change', getMoviesByGenre);
 
 // When form is submitted, run sendSearch
 searchButton.addEventListener('click', sendSearch);
