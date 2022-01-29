8a41fa8edc464eadc835f6b04376ff453ad558d0
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-29 10:47:12 -0600

    Search for movie by genre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var searchInputEl = document.querySelector("#search");
 var searchButton = document.querySelector("#run-search");
 var genreListEl = document.querySelector("#genre");
+var worstItemEl = document.querySelector(".item");
+
+
+
+
 
 var sendSearch = function (event) {
     // Prevent page from reloading which is the browser default action on form submit
@@ -15,8 +20,27 @@ var sendSearch = function (event) {
 
 var getMoviesByGenre = function (event) {
     var genre = genreListEl.value;
+
+    var apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=c930372b21a65386f628c5e6b7d65d66&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&primary_release_year=2021&vote_count.gte=30&with_genres=' + genre + '&with_watch_monetization_types=flatrate';
+
     console.log(genre);
-}
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            
+
+            document.querySelector("#actionflopheader").textContent = data.results[0].title;
+            document.querySelector("#worstimage").setAttribute('src', 'https://image.tmdb.org/t/p/w154' + data.results[0].poster_path);
+
+        })
+
+        worstItemEl.setAttribute('style', "display: flex");
+};
 
 // When an option on the list is selected
 genreListEl.addEventListener('change', getMoviesByGenre);
